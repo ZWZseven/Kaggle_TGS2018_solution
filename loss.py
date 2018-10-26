8a81f8ab0cbca0f6bff8fd12bf7443c8ccdd6007
Wz Zhao <34325234+ZhaoWenzhao@users.noreply.github.com>
2018-10-26 10:42:45 +0800

    Create loss.py

---
+++ loss.py
@@ -0,0 +1,457 @@
+   """
+    def set_loss(self):
+        if self.activation_func == 'softmax':
+            loss_function = partial(mixed_dice_cross_entropy_loss,
+                                    dice_loss=multiclass_dice_loss,
+                                    cross_entropy_loss=nn.CrossEntropyLoss(),
+                                    dice_activation='softmax',
+                                    dice_weight=self.architecture_config['model_params']['dice_weight'],
+                                    cross_entropy_weight=self.architecture_config['model_params']['bce_weight']
+                                    )
+        elif self.activation_func == 'sigmoid':
+            loss_function = partial(mixed_dice_bce_loss,
+                                    dice_loss=multiclass_dice_loss,
+                                    bce_loss=nn.BCEWithLogitsLoss(),
+                                    dice_activation='sigmoid',
+                                    dice_weight=self.architecture_config['model_params']['dice_weight'],
+                                    bce_weight=self.architecture_config['model_params']['bce_weight']
+                                    )
+        else:
+            raise Exception('Only softmax and sigmoid activations are allowed')
+        self.loss_function = [('mask', loss_function, 1.0)]
+"""
+class DiceLoss(nn.Module):
+    def __init__(self, smooth=0, eps=1e-7):
+        super(DiceLoss, self).__init__()
+        self.smooth = smooth
+        self.eps = eps
+
+    def forward(self, output, target):
+        return 1 - (2 * torch.sum(output * target) + self.smooth) / (
+                torch.sum(output) + torch.sum(target) + self.smooth + self.eps)
+
+
+def mixed_dice_bce_loss(output, target, dice_weight=0.2, dice_loss=None,
+                        bce_weight=0.9, bce_loss=None,
+                        smooth=0, dice_activation='sigmoid'):
+    num_classes = output.size(1)
+    target = target[:, :num_classes, :, :].long()
+    if bce_loss is None:
+        bce_loss = nn.BCEWithLogitsLoss()
+    if dice_loss is None:
+        dice_loss = multiclass_dice_loss
+    return dice_weight * dice_loss(output, target, smooth, dice_activation) + bce_weight * bce_loss(output, target)
+
+
+def mixed_dice_cross_entropy_loss(output, target, dice_weight=0.5, dice_loss=None,
+                                  cross_entropy_weight=0.5, cross_entropy_loss=None, smooth=0,
+                                  dice_activation='softmax'):
+    num_classes_without_background = output.size(1) - 1
+    dice_output = output[:, 1:, :, :]
+    dice_target = target[:, :num_classes_without_background, :, :].long()
+    cross_entropy_target = torch.zeros_like(target[:, 0, :, :]).long()
+    for class_nr in range(num_classes_without_background):
+        cross_entropy_target = where(target[:, class_nr, :, :], class_nr + 1, cross_entropy_target)
+    if cross_entropy_loss is None:
+        cross_entropy_loss = nn.CrossEntropyLoss()
+    if dice_loss is None:
+        dice_loss = multiclass_dice_loss
+    return dice_weight * dice_loss(dice_output, dice_target, smooth,
+                                   dice_activation) + cross_entropy_weight * cross_entropy_loss(output,
+                                                                                                cross_entropy_target)
+
+
+def multiclass_dice_loss(output, target, smooth=0, activation='softmax'):
+    """Calculate Dice Loss for multiple class output.
+    Args:
+        output (torch.Tensor): Model output of shape (N x C x H x W).
+        target (torch.Tensor): Target of shape (N x H x W).
+        smooth (float, optional): Smoothing factor. Defaults to 0.
+        activation (string, optional): Name of the activation function, softmax or sigmoid. Defaults to 'softmax'.
+    Returns:
+        torch.Tensor: Loss value.
+    """
+    if activation == 'softmax':
+        activation_nn = torch.nn.Softmax2d()
+    elif activation == 'sigmoid':
+        activation_nn = torch.nn.Sigmoid()
+    else:
+        raise NotImplementedError('only sigmoid and softmax are implemented')
+
+    loss = 0
+    dice = DiceLoss(smooth=smooth)
+    output = activation_nn(output)
+    num_classes = output.size(1)
+    target.data = target.data.float()
+    for class_nr in range(num_classes):
+        loss += dice(output[:, class_nr, :, :], target[:, class_nr, :, :])
+    return loss / num_classes
+
+
+def where(cond, x_1, x_2):
+    cond = cond.long()
+    return (cond * x_1) + ((1 - cond) * x_2)
+
+
+ class LossBinary:
+    """
+     Implementation from  https://github.com/ternaus/robot-surgery-segmentation
+    """
+
+    def __init__(self, jaccard_weight=0):
+        self.nll_loss = nn.BCEWithLogitsLoss()
+        self.jaccard_weight = jaccard_weight
+
+    def __call__(self, outputs, targets):
+        loss = self.nll_loss(outputs, targets)
+
+        if self.jaccard_weight:
+            eps = 1e-15
+            jaccard_target = (targets == 1.0).float()
+            jaccard_output = F.sigmoid(outputs)
+
+            intersection = (jaccard_output * jaccard_target).sum()
+            union = jaccard_output.sum() + jaccard_target.sum()
+
+            loss -= self.jaccard_weight * torch.log((intersection + eps) / (union - intersection + eps))
+        return loss
+
+
+from __future__ import print_function, division
+
+import torch
+from torch.autograd import Variable
+import torch.nn.functional as F
+import numpy as np
+try:
+    from itertools import  ifilterfalse
+except ImportError: # py3k
+    from itertools import  filterfalse
+
+
+def lovasz_grad(gt_sorted):
+    """
+    Computes gradient of the Lovasz extension w.r.t sorted errors
+    See Alg. 1 in paper
+    """
+    p = len(gt_sorted)
+    gts = gt_sorted.sum()
+    intersection = gts - gt_sorted.float().cumsum(0)
+    union = gts + (1 - gt_sorted).float().cumsum(0)
+    jaccard = 1. - intersection / union
+    if p > 1: # cover 1-pixel case
+        jaccard[1:p] = jaccard[1:p] - jaccard[0:-1]
+    return jaccard
+
+
+def iou_binary(preds, labels, EMPTY=1., ignore=None, per_image=True):
+    """
+    IoU for foreground class
+    binary: 1 foreground, 0 background
+    """
+    if not per_image:
+        preds, labels = (preds,), (labels,)
+    ious = []
+    for pred, label in zip(preds, labels):
+        intersection = ((label == 1) & (pred == 1)).sum()
+        union = ((label == 1) | ((pred == 1) & (label != ignore))).sum()
+        if not union:
+            iou = EMPTY
+        else:
+            iou = float(intersection) / union
+        ious.append(iou)
+    iou = mean(ious)    # mean accross images if per_image
+    return 100 * iou
+
+
+def iou(preds, labels, C, EMPTY=1., ignore=None, per_image=False):
+    """
+    Array of IoU for each (non ignored) class
+    """
+    if not per_image:
+        preds, labels = (preds,), (labels,)
+    ious = []
+    for pred, label in zip(preds, labels):
+        iou = []    
+        for i in range(C):
+            if i != ignore: # The ignored label is sometimes among predicted classes (ENet - CityScapes)
+                intersection = ((label == i) & (pred == i)).sum()
+                union = ((label == i) | ((pred == i) & (label != ignore))).sum()
+                if not union:
+                    iou.append(EMPTY)
+                else:
+                    iou.append(float(intersection) / union)
+        ious.append(iou)
+    ious = map(mean, zip(*ious)) # mean accross images if per_image
+    return 100 * np.array(ious)
+
+
+# --------------------------- BINARY LOSSES ---------------------------
+
+
+def lovasz_hinge(logits, labels, per_image=False, ignore=None):
+    """
+    Binary Lovasz hinge loss
+      logits: [B, H, W] Variable, logits at each pixel (between -\infty and +\infty)
+      labels: [B, H, W] Tensor, binary ground truth masks (0 or 1)
+      per_image: compute the loss per image instead of per batch
+      ignore: void class id
+    """
+    if per_image:
+        loss = mean(lovasz_hinge_flat(*flatten_binary_scores(log.unsqueeze(0), lab.unsqueeze(0), ignore))
+                          for log, lab in zip(logits, labels))
+    else:
+        loss = lovasz_hinge_flat(*flatten_binary_scores(logits, labels, ignore))
+    return loss
+
+
+def lovasz_hinge_flat(logits, labels):
+    """
+    Binary Lovasz hinge loss
+      logits: [P] Variable, logits at each prediction (between -\infty and +\infty)
+      labels: [P] Tensor, binary ground truth labels (0 or 1)
+      ignore: label to ignore
+    """
+    if len(labels) == 0:
+        # only void pixels, the gradients should be 0
+        return logits.sum() * 0.
+    signs = 2. * labels.float() - 1.
+    errors = (1. - logits * signs)#Variable(signs))############
+    errors_sorted, perm = torch.sort(errors, dim=0, descending=True)
+    perm = perm.data
+    gt_sorted = labels[perm]
+    grad = lovasz_grad(gt_sorted)
+    #loss = torch.dot(F.elu(errors_sorted)+1,  grad)#Variable(grad))#ELU+1
+    loss = torch.dot(F.relu(errors_sorted),  grad)#Variable(grad))#RELU
+    return loss
+
+
+def flatten_binary_scores(scores, labels, ignore=None):
+    """
+    Flattens predictions in the batch (binary case)
+    Remove labels equal to 'ignore'
+    """
+    scores = scores.view(-1)
+    labels = labels.view(-1)
+    if ignore is None:
+        return scores, labels
+    valid = (labels != ignore)
+    vscores = scores[valid]
+    vlabels = labels[valid]
+    return vscores, vlabels
+
+
+class StableBCELoss(torch.nn.modules.Module):
+    def __init__(self):
+         super(StableBCELoss, self).__init__()
+    def forward(self, input, target):
+         neg_abs = - input.abs()
+         loss = input.clamp(min=0) - input * target + (1 + neg_abs.exp()).log()
+         return loss.mean()
+
+
+def binary_xloss(logits, labels, ignore=None):
+    """
+    Binary Cross entropy loss
+      logits: [B, H, W] Variable, logits at each pixel (between -\infty and +\infty)
+      labels: [B, H, W] Tensor, binary ground truth masks (0 or 1)
+      ignore: void class id
+    """
+    logits, labels = flatten_binary_scores(logits, labels, ignore)
+    loss = StableBCELoss()(logits, Variable(labels.float()))
+    return loss
+
+
+# --------------------------- MULTICLASS LOSSES ---------------------------
+
+
+def lovasz_softmax(probas, labels, only_present=False, per_image=False, ignore=None):
+    """
+    Multi-class Lovasz-Softmax loss
+      probas: [B, C, H, W] Variable, class probabilities at each prediction (between 0 and 1)
+      labels: [B, H, W] Tensor, ground truth labels (between 0 and C - 1)
+      only_present: average only on classes present in ground truth
+      per_image: compute the loss per image instead of per batch
+      ignore: void class labels
+    """
+    if per_image:
+        loss = mean(lovasz_softmax_flat(*flatten_probas(prob.unsqueeze(0), lab.unsqueeze(0), ignore), only_present=only_present)
+                          for prob, lab in zip(probas, labels))
+    else:
+        loss = lovasz_softmax_flat(*flatten_probas(probas, labels, ignore), only_present=only_present)
+    return loss
+
+
+def lovasz_softmax_flat(probas, labels, only_present=False):
+    """
+    Multi-class Lovasz-Softmax loss
+      probas: [P, C] Variable, class probabilities at each prediction (between 0 and 1)
+      labels: [P] Tensor, ground truth labels (between 0 and C - 1)
+      only_present: average only on classes present in ground truth
+    """
+    C = probas.size(1)
+    losses = []
+    for c in range(C):
+        fg = (labels == c).float() # foreground for class c
+        if only_present and fg.sum() == 0:
+            continue
+        errors = (fg - probas[:, c]).abs()#(Variable(fg) - probas[:, c]).abs()#############################
+        errors_sorted, perm = torch.sort(errors, 0, descending=True)
+        perm = perm.data
+        fg_sorted = fg[perm]
+        losses.append(torch.dot(errors_sorted, lovasz_grad(fg_sorted)))#Variable(lovasz_grad(fg_sorted))))
+    return mean(losses)
+
+
+def flatten_probas(probas, labels, ignore=None):
+    """
+    Flattens predictions in the batch
+    """
+    B, C, H, W = probas.size()
+    probas = probas.permute(0, 2, 3, 1).contiguous().view(-1, C)  # B * H * W, C = P, C
+    labels = labels.view(-1)
+    if ignore is None:
+        return probas, labels
+    valid = (labels != ignore)
+    vprobas = probas[valid.nonzero().squeeze()]
+    vlabels = labels[valid]
+    return vprobas, vlabels
+
+def xloss(logits, labels, ignore=None):
+    """
+    Cross entropy loss
+    """
+    return F.cross_entropy(logits, Variable(labels), ignore_index=255)
+
+
+# --------------------------- HELPER FUNCTIONS ---------------------------
+
+def mean(l, ignore_nan=False, empty=0):
+    """
+    nanmean compatible with generators.
+    """
+    l = iter(l)
+    if ignore_nan:
+        l = ifilterfalse(np.isnan, l)
+    try:
+        n = 1
+        acc = next(l)
+    except StopIteration:
+        if empty == 'raise':
+            raise ValueError('Empty mean')
+        return empty
+    for n, v in enumerate(l, 2):
+        acc += v
+    if n == 1:
+        return acc
+    return acc / n
+
+def lovasz_loss(outputs,targets):
+    return lovasz_hinge(outputs,targets)
+#lovasz_softmax(torch.from_numpy(outputs.data.numpy()),torch.from_numpy(targets.data.numpy()))
+
+class LossBinary1:
+    """
+     Implementation from  https://github.com/ternaus/robot-surgery-segmentation
+    """
+
+    def __init__(self, jaccard_weight=0):
+        self.nll_loss = lovasz_loss#mixed_dice_bce_loss#lovasz_loss#lovasz_softmax#nn.BCEWithLogitsLoss()
+        self.jaccard_weight = jaccard_weight
+
+    def __call__(self, outputs, targets):
+        #print(outputs.shape)
+        #print(targets.shape)
+        loss = self.nll_loss(outputs, targets)
+
+        if self.jaccard_weight:
+            eps = 1e-15
+            jaccard_target = (targets == 1.0).float()
+            jaccard_output = F.sigmoid(outputs)
+
+            intersection = (jaccard_output * jaccard_target).sum()
+            union = jaccard_output.sum() + jaccard_target.sum()
+
+            loss -= self.jaccard_weight * torch.log((intersection + eps) / (union - intersection + eps))
+        return loss
+
+def lovasz_hingeElu(logits, labels, per_image=False, ignore=None):
+    """
+    Binary Lovasz hinge loss
+      logits: [B, H, W] Variable, logits at each pixel (between -\infty and +\infty)
+      labels: [B, H, W] Tensor, binary ground truth masks (0 or 1)
+      per_image: compute the loss per image instead of per batch
+      ignore: void class id
+    """
+    if per_image:
+        loss = mean(lovasz_hinge_flatElu(*flatten_binary_scores(log.unsqueeze(0), lab.unsqueeze(0), ignore))
+                          for log, lab in zip(logits, labels))
+    else:
+        loss = lovasz_hinge_flatElu(*flatten_binary_scores(logits, labels, ignore))
+    return loss
+
+
+def lovasz_hinge_flatElu(logits, labels):
+    """
+    Binary Lovasz hinge loss
+      logits: [P] Variable, logits at each prediction (between -\infty and +\infty)
+      labels: [P] Tensor, binary ground truth labels (0 or 1)
+      ignore: label to ignore
+    """
+    if len(labels) == 0:
+        # only void pixels, the gradients should be 0
+        return logits.sum() * 0.
+    signs = 2. * labels.float() - 1.
+    errors = (1. - logits * signs)#Variable(signs))############
+    errors_sorted, perm = torch.sort(errors, dim=0, descending=True)
+    perm = perm.data
+    gt_sorted = labels[perm]
+    grad = lovasz_grad(gt_sorted)
+    loss = torch.dot(F.elu(errors_sorted)+1,  grad)#Variable(grad))#ELU+1
+    #loss = torch.dot(F.relu(errors_sorted),  grad)#Variable(grad))#RELU
+    return loss
+
+class LossBinaryElu:
+    """
+     Implementation from  https://github.com/ternaus/robot-surgery-segmentation
+    """
+
+    def __init__(self, jaccard_weight=0):
+        self.nll_loss = lovasz_hingeElu#lovasz_loss#mixed_dice_bce_loss#lovasz_loss#lovasz_softmax#nn.BCEWithLogitsLoss()
+        self.jaccard_weight = jaccard_weight
+
+    def __call__(self, outputs, targets):
+        #print(outputs.shape)
+        #print(targets.shape)
+        loss = self.nll_loss(outputs, targets)
+
+        if self.jaccard_weight:
+            eps = 1e-15
+            jaccard_target = (targets == 1.0).float()
+            jaccard_output = F.sigmoid(outputs)
+
+            intersection = (jaccard_output * jaccard_target).sum()
+            union = jaccard_output.sum() + jaccard_target.sum()
+
+            loss -= self.jaccard_weight * torch.log((intersection + eps) / (union - intersection + eps))
+        return loss
+
+
+class FocalLoss(nn.Module):
+    def __init__(self, gamma):
+        super().__init__()
+        self.gamma = gamma
+        
+    def forward(self, input, target):
+        # Inspired by the implementation of binary_cross_entropy_with_logits
+        if not (target.size() == input.size()):
+            raise ValueError("Target size ({}) must be the same as input size ({})".format(target.size(), input.size()))
+
+        max_val = (-input).clamp(min=0)
+        loss = input - input * target + max_val + ((-max_val).exp() + (-input - max_val).exp()).log()
+
+        # This formula gives us the log sigmoid of 1-p if y is 0 and of p if y is 1
+        invprobs = F.logsigmoid(-input * (target * 2 - 1))
+        loss = (invprobs * self.gamma).exp() * loss
+        
+        return loss.mean()
